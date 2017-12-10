f8468997e3f1dd50373781a5e4b0bcbed9b90cf9
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-12-10 14:47:04 -0800

    Add episodes_controller#index test

--- test/controllers/episodes_controller_test.rb
+++ test/controllers/episodes_controller_test.rb
@@ -4,4 +4,15 @@ class EpisodesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not get index without authentication" do
+    get episodes_url, as: :json
+    assert_response :unauthorized
+  end
+
+  test "should get index with authentication" do
+    user = users(:one)
+    get episodes_url, as: :json, headers: { Authorization: "Token token=#{user.token}" }
+    assert_response :success
+  end
 end
