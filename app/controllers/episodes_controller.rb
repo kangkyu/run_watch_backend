0de15ecd542f0913189ed13dc78fac4b0a71a197
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-01-12 16:45:49 -0800

    Add UI to crud episodes

---
+++ app/controllers/episodes_controller.rb
@@ -0,0 +1,42 @@
+class EpisodesController < ApplicationController
+  def index
+    @episodes = Episode.all
+  end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = Episode.new(episode_params)
+    if @episode.save
+      redirect_to episodes_url, notice: 'Episode was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @episode = Episode.find(params[:id])
+  end
+
+  def update
+    @episode = Episode.find(params[:id])
+    if @episode.update(episode_params)
+      redirect_to episodes_url, notice: 'Episode was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @episode = Episode.find(params[:id])
+    @episode.destroy
+    redirect_to episodes_url, notice: 'Episode was successfully destroyed.'
+  end
+
+private
+  def episode_params
+    params.require(:episode).permit(:title, :description, :number)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
       resources :episodes, only: [:index, :update, :show, :create]
     end
   end
+
+  resources :episodes
 end
