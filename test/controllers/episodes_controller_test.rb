4debd70222b3a3f7a45221957c9b1814271f7b1f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-01-12 16:54:27 -0800

    update test

--- test/controllers/episodes_controller_test.rb
+++ test/controllers/episodes_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class EpisodesControllerTest < ActionDispatch::IntegrationTest
 
   test "should not get index without token" do
-    get episodes_url, as: :json
+    get api_episodes_url, as: :json
     assert_response :unauthorized
   end
 
   test "should get index with token" do
     user = users(:one)
-    get episodes_url, as: :json, headers: { Authorization: "Token token=#{user.token}" }
+    get api_episodes_url, as: :json, headers: { Authorization: "Token token=#{user.token}" }
     assert_response :success
   end
 end
